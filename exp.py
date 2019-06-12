29c880c1f456e55f2d3d33862ed0573d68dd353c
aldnoahh <34053015+aldnoahh@users.noreply.github.com>
2019-06-12 16:12:34 +0530

    Update exp.py

--- exp.py
+++ exp.py
@@ -1,33 +1,22 @@
+#Program for spliting multiple audio files into sub-file of each with a defined time
+
+
+
 from pydub import AudioSegment as aseg
 import os
-mylist= os.listdir('/home/akshay/Desktop/audio/')
+mylist= os.listdir('/home/akshay/Desktop/audio/')  #input directory
 
 type(mylist)
-
+t=5   #define the time you need each file to be
 for index, y in enumerate(mylist):
 	nAud = aseg.from_wav("/home/akshay/Desktop/audio/"+y)
 	k=int(len(nAud)/1000)
-	y1= (k//5)+1
+	y1= (k//t)+1
+	a1,a2=0,t
 	for j in range(y1+1):
-		a1,a2=0,5
 		t1 = a1 * 1000 #Works in milliseconds
 		t2 = a2 * 1000
-		a1+=5
-		a2+=5
+		a1+=t
+		a2+=t
 		nAud = nAud[t1:t2]
-		nAud.export('/home/akshay/Desktop/audi2/'+y+str(j)+'.WAV')
-		#nAud.export('/home/akshay/Desktop/audi/'+y+str(j), format="wav")
-
-
-
-
-
-"""
-t1 = 0 * 1000 #Works in milliseconds
-t2 = 10 * 1000
-nAud = aseg.from_wav("/home/akshay/Desktop/audio/1An.WAV")
-nAud = newAudio[t1:t2]
-nAud.export('/home/akshay/Desktop/s_half.WAV', format="wav") """
-		
-	
-
+		nAud.export('/home/akshay/Desktop/audi2/'+y+str(j)+'.WAV')  #output directory
